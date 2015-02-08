c27536911d7b7ed456e882257173c5db1e24c330
Sony Theakanath <stheakanath@gmail.com>
2015-02-08 13:00:49 -0800

    added parse sdk to app

--- Malibu/AppDelegate.m
+++ Malibu/AppDelegate.m
@@ -7,11 +7,29 @@
 //
 
 #import "AppDelegate.h"
+#import <Parse/Parse.h>
 
 @interface AppDelegate ()
 
 @end
 
 @implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // [Optional] Power your app with Local Datastore. For more info, go to
+    // https://parse.com/docs/ios_guide#localdatastore/iOS
+    [Parse enableLocalDatastore];
+    
+    // Initialize Parse.
+    [Parse setApplicationId:@"bzlyG2EdywDSk6ZvBe6oLDo8Isbd1wSa4EIJLext"
+                  clientKey:@"1ECEr3PTSLWNfFFrZaehMg2PPiSVYRWwOlriPy02"];
+    
+    // [Optional] Track statistics around application opens.
+    [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
+    
+    // ...
+    return YES;
+}
 
 @end
