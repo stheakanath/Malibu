88a9f082a8bb8374a87b1aa0f0f564743255fa05
Sony Theakanath <stheakanath@gmail.com>
2015-02-08 13:11:50 -0800

    region controller

---
+++ Malibu/RegionController.h
@@ -0,0 +1,13 @@
+//
+//  RegionController.h
+//  Malibu
+//
+//  Created by Sony Theakanath on 2/8/15.
+//  Copyright (c) 2015 Sony Theakanath. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RegionController : NSObject
+
+@end
---
+++ Malibu/RegionController.m
@@ -0,0 +1,13 @@
+//
+//  RegionController.m
+//  Malibu
+//
+//  Created by Sony Theakanath on 2/8/15.
+//  Copyright (c) 2015 Sony Theakanath. All rights reserved.
+//
+
+#import "RegionController.h"
+
+@implementation RegionController
+
+@end
