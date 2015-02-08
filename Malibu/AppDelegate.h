4ea2b24e48ce5ebdaf4699219495da700251a0f2
Sony Theakanath <stheakanath@gmail.com>
2015-02-08 12:47:20 -0800

    Initial Commit

---
+++ Malibu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Malibu
+//
+//  Created by Sony Theakanath on 2/8/15.
+//  Copyright (c) 2015 Sony Theakanath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Malibu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Malibu
+//
+//  Created by Sony Theakanath on 2/8/15.
+//  Copyright (c) 2015 Sony Theakanath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
