51bfb9113b97cf7dcbad453add70a5c6cb4b1aea
Sony Theakanath <stheakanath@gmail.com>
2015-02-08 16:00:27 -0800

    Cleanup of Beacon.m header file

--- Malibu/Beacon.m
+++ Malibu/Beacon.m
@@ -1,6 +1,6 @@
 //
-//  BeaconManager.m
-//  BrightonDome
+//  Beacon.m
+//  Malibu
 //
 //  Created by Sony Theakanath & Saurabh Sharan on 2/8/15.
 //  Copyright (c) 2015 Sony Theakanath. All rights reserved.
